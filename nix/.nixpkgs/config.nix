fbee37e8a95186df9af02272297840c943a0d4fb
Leroy Hopson <git@leroy.geek.nz>
2015-12-06 23:06:10 +1300

    Selectively load latex-live-preview plugin
    Make it so the latex-live-preview plugin is only loaded when editing
    tex files.

--- nix/.nixpkgs/config.nix
+++ nix/.nixpkgs/config.nix
@@ -6,20 +6,24 @@ with pkgs; rec {
   vimrcConfig = {
     vam.knownPlugins = vimPlugins;
     vam.pluginDictionaries = [
+      # load always
       { names = [
           "CSApprox"
           "Colour-Sampler-Pack"
           "changeColorScheme-vim"
           "fugitive"
-          "latex-live-preview"
           "random-vim"
           "sleuth"
           "vim-addon-vim2nix"
-        ]; }
+        ];
+      }
+      # load only when opening a .tex file
+      { name = "latex-live-preview"; ft_regex = "^tex\$"; }
+      { name = "latex-live-preview"; filename_regex = "^tex\$"; }
     ];
     customRC = vimrc.config;
   };
-  
+
   my_vim = vim_configurable.customize { name = "vim"; inherit vimrcConfig; };
 
   packageOverrides = pkgs_: with pkgs_; {
